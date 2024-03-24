cf4ce5cf26e6abb6cace548a141dc4815439eb85
Dark-GreenCat <tranquanghuy3008@outlook.com.vn>
2024-03-24 23:44:35 +0700

    .

--- src/task2_1.c
+++ src/task2_1.c
@@ -5,7 +5,7 @@ int main() {
     float b = 3.14;
     char c = 97;
     unsigned short d = 128;
-    char str[] = "Hello World! ";
+    char str[] = "Hello World!.";
 
     printf("%d\n%.4f\n%c\n%hu\n%s", a, b, c, d, str);
 }
